40326cad9c1d969a85d89fe1c4c1cb96cff2fbe1
Tom Barnes <barnes.thomas.c@gmail.com>
2018-08-14 14:48:52 -0400

    carousel fully functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,14 @@ Vibe.create(name: 'Latest', user_id: 1);
 Vibe.create(name: 'Fuego', user_id: 1);
 Vibe.create(name: 'Latest', user_id: 2);
 Vibe.create(name: 'Jazz Cabbage', user_id: 2);
+Vibe.create(name: 'New Live Evil', user_id: 2);
 
 Post.create(title: 'Trap Disneyland?', song_uri: '<iframe src="https://open.spotify.com/embed/track/7wBJfHzpfI3032CSD7CE2m" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>', post_body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
 Post.create(title: 'Kamasi\'s Dream', song_uri: '<iframe src="https://open.spotify.com/embed/track/2kTWcP2fid9TKLdcRuiAq4" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>', post_body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
 Post.create(title: 'Space Cadillacs', song_uri: '<iframe src="https://open.spotify.com/embed/track/4UIfBRnJoiGeQhBec0mHm0" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>', post_body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
 Post.create(title: 'Imani\'s Vibe', song_uri: '<iframe src="https://open.spotify.com/embed/track/5IIq5uEYpUZoSjTEjqn7q1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>', post_body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
+Post.create(title: 'What I Say?', song_uri: '<iframe src="https://open.spotify.com/embed/track/4XTtGRL6dJgXm3TWWwEFDU" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>', post_body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
+Post.create(title: 'Inamorata 2', song_uri: '<iframe src="https://open.spotify.com/embed/track/6Nmmmpujq5cbqhKOcu7qyb" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>', post_body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
 
 
 Tag.create(name: 'Hip-hop');
@@ -25,6 +28,10 @@ VibeConnection.create(vibe_id: 3, post_id: 3);
 VibeConnection.create(vibe_id: 3, post_id: 4);
 VibeConnection.create(vibe_id: 4, post_id: 3);
 VibeConnection.create(vibe_id: 4, post_id: 4);
+VibeConnection.create(vibe_id: 5, post_id: 5);
+VibeConnection.create(vibe_id: 5, post_id: 6);
+VibeConnection.create(vibe_id: 3, post_id: 5);
+VibeConnection.create(vibe_id: 3, post_id: 6);
 
 
 TagConnection.create(vibe_id: 1, tag_id: 1);
@@ -35,3 +42,5 @@ TagConnection.create(vibe_id: 3, tag_id: 3);
 TagConnection.create(vibe_id: 3, tag_id: 4);
 TagConnection.create(vibe_id: 4, tag_id: 3);
 TagConnection.create(vibe_id: 4, tag_id: 4);
+TagConnection.create(vibe_id: 5, tag_id: 3);
+TagConnection.create(vibe_id: 5, tag_id: 4);
