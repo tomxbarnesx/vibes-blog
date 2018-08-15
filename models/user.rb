609657f2ce53f5bf1d7b7d79746ac0e4050b5186
Tom Barnes <barnes.thomas.c@gmail.com>
2018-08-15 16:12:43 -0400

    edit styling complete

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-    has_many :vibes
+    has_many :vibes, :dependent => :destroy
 end
--- models/vibe.rb
+++ models/vibe.rb
@@ -2,5 +2,6 @@ class Vibe < ActiveRecord::Base
     belongs_to :user
     has_many :vibe_connections
     has_many :posts, through: :vibe_connections
+    has_many :tag_connections
     has_many :tags, through: :tag_connections
 end
