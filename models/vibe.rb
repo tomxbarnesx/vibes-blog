d075f5d266eb0c60ae23f4dab4bd398e1c57a637
Tom Barnes <barnes.thomas.c@gmail.com>
2018-08-15 18:30:51 -0400

    more progress on the edit features

--- models/vibe.rb
+++ models/vibe.rb
@@ -1,7 +1,7 @@
 class Vibe < ActiveRecord::Base
     belongs_to :user
-    has_many :vibe_connections
+    has_many :vibe_connections, :dependent => :destroy
     has_many :posts, through: :vibe_connections
-    has_many :tag_connections
+    has_many :tag_connections, :dependent => :destroy
     has_many :tags, through: :tag_connections
 end
